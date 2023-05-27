1b7cf9fe8560d0b8eb0778fccb91f79bc2a444a1
meliodas560 <134010249+meliodas560@users.noreply.github.com>
2023-05-28 03:57:29 +0700

    update GreyCilik/modules/alive.py

--- GreyCilik/modules/alive.py
+++ GreyCilik/modules/alive.py
@@ -8,22 +8,22 @@ from pyrogram import __version__ as pyrover
 from GreyCilik.events import register
 from GreyCilik import telethn as tbot
 
-CILIK = "https://telegra.ph//file/6f7d2641d558c8d88431f.jpg"
+CILIK = "https://telegra.ph/file/8978a0ec918ac1f045ae5.jpg"
 
-PHOTO = "https://telegra.ph//file/6f7d2641d558c8d88431f.jpg"
+PHOTO = "https://telegra.ph/file/8978a0ec918ac1f045ae5.jpg"
 
 QRIS = "https://telegra.ph//file/6ea96dc45b358a7aa2151.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  GREY = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm RED LINE SEX.** \n\n"
+  GREY = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm ᴍᴇʟɪᴏᴅλs ꭙ ꝛᴏʙᴏᴛ.** \n\n"
   GREY += "⚡️ **I'm Working Properly** \n\n"
-  GREY += f"⚡️ **My Master : [MASTER](https://t.me/yankeseleo)** \n\n"
+  GREY += f"⚡️ **My Master : [MASTER](https://t.me/berisikjeleg)** \n\n"
   GREY += f"⚡️ **Library Version :** `{telever}` \n\n"
   GREY += f"⚡️ **Telethon Version :** `{tlhver}` \n\n"
   GREY += f"⚡️ **Pyrogram Version :** `{pyrover}` \n\n"
   GREY += "**Thanks For Adding Me Here My Love ❤️**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/asupanrlsbot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/+uSY5LHBBqe9mODUx")]]
+  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/cemarasupport?start=help"), Button.url("sᴜᴘsᴜᴘᴘᴏʀ", "https://t.me/+Tp5IJIq4NjIyYWE1")]
   await tbot.send_file(event.chat_id, PHOTO, caption=GREY,  buttons=BUTTON)
 
 
@@ -42,7 +42,7 @@ async def awake(event):
   RISA += "✪ `/f8 <text> (𝕔𝕚𝕝𝕚𝕜)` \n"
   RISA += "✪ `/f9 <text> (ᴄɪʟɪᴋ)` \n"
   RISA += "✪ `/f10 <text> (𝐂𝐈𝐋𝐈𝐊)`"
-  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/lordilhamxrobot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/libitrashh")]]
+  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/meliodaskunrobot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/+Tp5IJIq4NjIyYWE1")]]
   await tbot.send_file(event.chat_id, CILIK, caption=RISA,  buttons=BUTTON)
 
 
